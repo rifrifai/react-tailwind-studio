9012f7cc5d42ace85b31c95bc42cc02960447572
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-06-11 09:18:06 +0700

    preparation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center bg-blue-600 text-slate-600">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
