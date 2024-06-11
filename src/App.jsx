abd8e389ffb2c5269d89aad5e213f7a6ce1c92cb
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-06-11 10:18:47 +0700

    component and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
+/* eslint-disable react/prop-types */
+// function Button() {
+//   return (
+//     <button
+//       className="h-10 m-4 px-6 font-semibold rounded-full bg-yellow-600 text-white"
+//       type="submit"
+//     >
+//       Buy now!
+//     </button>
+//   );
+// }
+
+// import React from "react";
+// class ButtonGray extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <button
+//           className="h-10 m-4 px-6 font-semibold rounded-full bg-gray-600 text-white"
+//           type="submit"
+//         >
+//           Buy now!
+//         </button>
+//       </>
+//     );
+//   }
+// }
+
+const Button = ({ color, text }) => {
+  return (
+    <>
+      <button
+        className={`h-10 m-4 px-6 font-semibold rounded-full ${color} text-white`}
+        type="submit"
+      >
+        {text}
+      </button>
+    </>
+  );
+};
+
 function App() {
   return (
     <>
-      <h1 className="flex justify-center bg-blue-600 text-slate-600">
-        Hello World
-      </h1>
+      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+        <Button color={"bg-red-600"} text={"Login"} />
+        <Button color={"bg-purple-600"} text={"Logout"} />
+        <Button color={"bg-gray-600"} text={"Register"} />
+      </div>
     </>
   );
 }
