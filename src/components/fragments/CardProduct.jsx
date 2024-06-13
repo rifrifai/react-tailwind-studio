0d6119430145e1188167a9245df04be22c3b6965
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-06-13 11:35:31 +0700

    rendering list final

---
+++ src/components/fragments/CardProduct.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+export default function CardProduct({ children }) {
+  return (
+    <>
+      <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
+        {children}
+      </div>
+    </>
+  );
+}
+
+function Header({ image }) {
+  return (
+    <>
+      <a href="#">
+        <img src={image} alt="shoes-1" className="p-8 rounded-t-lg" />
+      </a>
+    </>
+  );
+}
+
+function Body({ children, name }) {
+  return (
+    <>
+      <div className="px-5 pb-5 h-full">
+        <a href="#">
+          <h5 className="text-xl font-semibold tracking-tight text-white">
+            {name}
+          </h5>
+          <p className="text-s text-white">{children}</p>
+        </a>
+      </div>
+    </>
+  );
+}
+
+function Footer({ price }) {
+  return (
+    <>
+      <div className="flex items-center justify-between px-5 pb-5">
+        <span className="text-xl font-bold text-white">{price}</span>
+        <button className="bg-blue-600 rounded p-2 text-white font-medium">
+          Add To Cart
+        </button>
+      </div>
+    </>
+  );
+}
+
+// dinamakan nested component
+CardProduct.Header = Header;
+CardProduct.Body = Body;
+CardProduct.Footer = Footer;
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,35 +1,55 @@
+import CardProduct from "../components/fragments/CardProduct";
+
+const products = [
+  {
+    id: 1,
+    name: "New Nike Air 1",
+    price: "Rp. 1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quae ad voluptatem maiores nobis esse, dolore animi rerum temporibus excepturi. Ipsa accusamus harum facilis quos ex consequatur sequi quasi ad?`,
+  },
+
+  {
+    id: 2,
+    name: "Adidas Black Current",
+    price: "Rp. 3.999.000",
+    image: "/images/shoes-2.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa accusamus harum facilis quos ex consequatur sequi quasi ad?`,
+  },
+  {
+    id: 3,
+    name: "Black Mamba Puma",
+    price: "Rp. 2.499.000",
+    image: "/images/shoes-3.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae obcaecati expedita minus adipisci cumque quaerat fuga, dolorem aliquam quia nulla. Quos quaerat labore natus, enim quae maxime dolore distinctio unde.`,
+  },
+];
 export default function ProductsPage() {
   return (
     <>
       <div className="flex justify-center py-5">
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
-          <a href="#">
-            <img
-              src="/public/images/shoes-1.jpg"
-              alt="shoes-1"
-              className="p-8 rounded-t-lg"
-            />
-          </a>
-          <div className="px-5 pb-5">
-            <a href="#">
-              <h5 className="text-xl font-semibold tracking-tight text-white">
-                New Nike Air 1
-              </h5>
-              <p className="text-s text-white">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-                quae ad voluptatem maiores nobis esse, dolore animi rerum
-                temporibus excepturi. Ipsa accusamus harum facilis quos ex
-                consequatur sequi quasi ad?
-              </p>
-            </a>
-          </div>
-          <div className="flex items-center justify-between px-5 pb-5">
-            <span className="text-xl font-bold text-white">Rp. 1.000.000</span>
-            <button className="bg-blue-600 rounded p-2 text-white font-medium">
-              Add To Cart
-            </button>
-          </div>
-        </div>
+        {/* nested component */}
+        {/* <CardProduct>
+          <CardProduct.Header image="/images/shoes-1.jpg" />
+          <CardProduct.Body title="New Nike Air 1">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quae
+            ad voluptatem maiores nobis esse, dolore animi rerum temporibus
+            excepturi. Ipsa accusamus harum facilis quos ex consequatur sequi
+            quasi ad?
+          </CardProduct.Body>
+          <CardProduct.Footer price="Rp. 1.000.000" />
+        </CardProduct> */}
+
+        {/* rendering list  */}
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image} />
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        ))}
       </div>
     </>
   );
