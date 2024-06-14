b6215a9cf2d5f27d8131b9ac6f8c9eb7feecc1f9
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-06-14 09:46:02 +0700

    on going to save in local storage

--- src/components/elements/button/Button.jsx
+++ src/components/elements/button/Button.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
 
-const Button = ({ text, classname = "bg-black" }) => {
+const Button = ({
+  text,
+  classname = "bg-black",
+  handleLogin = () => {},
+  type = "button",
+}) => {
   return (
     <>
       <button
         className={`h-10 px-6  font-semibolb rounded-md ${classname} text-white`}
-        type="button"
+        type={type}
+        onClick={() => {
+          handleLogin();
+        }}
       >
         {text}
       </button>
--- src/components/fragments/FormLogin.jsx
+++ src/components/fragments/FormLogin.jsx
@@ -3,9 +3,17 @@ import InputForm from "../elements/input/Index";
 import Button from "../elements/button/Button";
 
 export default function FormLogin() {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // untuk meng console data login
+    console.info(event.target.email.value);
+    console.info(event.target.password.value);
+    console.log("Login Successful");
+  };
+
   return (
     <>
-      <form action="">
+      <form onSubmit={handleLogin}>
         <InputForm
           label="Email"
           type="email"
@@ -18,7 +26,7 @@ export default function FormLogin() {
           placeholder="********"
           name="password"
         />
-        <Button text="Login" classname="bg-blue-600 w-full" />
+        <Button text="Login" classname="bg-blue-600 w-full" type="submit" />
       </form>
     </>
   );
