58d32e2a30608bad943e733738a02e2f078b28e5
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-06-15 08:42:51 +0700

    contructor state final

--- src/components/fragments/Counter.jsx
+++ src/components/fragments/Counter.jsx
@@ -10,8 +10,19 @@ class Counter extends React.Component {
   render() {
     return (
       <div className="flex items-center">
-        <p className="mr-5">Counter</p>
-        <button className="bg-black text-white p-3  rounded-md">+</button>
+        <button
+          className="bg-black text-white rounded-md p-3"
+          onClick={() => this.setState({ count: this.state.count - 1 })}
+        >
+          -
+        </button>
+        <p className="m-5">{this.state.count}</p>
+        <button
+          className="bg-black text-white p-3  rounded-md"
+          onClick={() => this.setState({ count: this.state.count + 1 })}
+        >
+          +
+        </button>
       </div>
     );
   }
