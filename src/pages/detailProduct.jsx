fcce6f430c38523658f2d999e0ea3e2e5fa480a7
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-07-10 22:17:23 +0700

    initial commit

--- src/pages/detailProduct.jsx
+++ src/pages/detailProduct.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getDetailProduct } from "../services/product.service";
