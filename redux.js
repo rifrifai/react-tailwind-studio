cb53a5c678354d913e55fd1b81ab5273c72712ff
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-07-16 23:45:05 +0700

    reducer

---
+++ redux.js
@@ -0,0 +1,18 @@
+// reducer
+const cartReducer = (state = { cart: [] }, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+// store
+
+// subcribe
+
+// dispatch
