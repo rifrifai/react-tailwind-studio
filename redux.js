06e66b8efba145a1256c34743957405af9bbe077
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-07-16 23:58:35 +0700

    subscribe

--- redux.js
+++ redux.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux";
 
 // reducer
-const cartReducer = (state = { cart: [] }, action) => {
+const cartReducer = (state = { cart: [{ id: 1, qty: 20 }] }, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
       return {
@@ -15,10 +15,16 @@ const cartReducer = (state = { cart: [] }, action) => {
 
 // store
 const store = createStore(cartReducer);
-console.info("oncreate store : ", store.getState());
+console.info("onCreate Store : ", store.getState());
 
-// subcribe
+// subscribe
+store.subscribe(() => {
+  console.info("Store Change : ", store.getState());
+});
 
 // dispatch
 const action1 = { type: "ADD_TO_CART", payload: { id: 2, qty: 20 } };
 store.dispatch(action1);
+
+const action2 = { type: "ADD_TO_CART", payload: { id: 10, qty: 5 } };
+store.dispatch(action2);
